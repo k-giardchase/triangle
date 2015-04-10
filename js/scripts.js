e6bba7efaab766a6c72a8e1fbbf19fd01b99a7e8
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-10 09:23:35 -0700

    Pass second test for isosceles triangle, a === b.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var triangleIdentifier = function(a, b, c) {
     if( a===b && a === c && b===c) {
         return "Equilateral";
+    } else if (a===b) {
+        return "Isosceles";
     }
 }
--- specs/specs.js
+++ specs/specs.js
@@ -4,7 +4,7 @@ describe('triangleIdentifier', function() {
         expect(triangleIdentifier(1,1,1)).to.equal('Equilateral');
     });
 
-    it('is an isosceles triangle for a user input where two side lengths are equal', function() {
-        expect(triangleIdentifier(1,2,1)).to.equal('Isosceles');
+    it('is an isosceles triangle for a user input where side lengths a and b are equal, but not c', function() {
+        expect(triangleIdentifier(1,1,2)).to.equal('Isosceles');
     });
 });
