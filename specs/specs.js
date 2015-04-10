bfb6152978e6c9302047b9707b2fe3385d7de40f
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-10 09:29:32 -0700

    Add test for isoscles triangle, b===c

--- specs/specs.js
+++ specs/specs.js
@@ -1,14 +1,18 @@
 describe('triangleIdentifier', function() {
 
     it('is an equilaterial triangle for a user input where all side lengths are equal', function() {
-        expect(triangleIdentifier(1,1,1)).to.equal('Equilateral');
+        expect(triangleIdentifier(3,3,3)).to.equal('Equilateral');
     });
 
     it('is an isosceles triangle for a user input where side lengths a and b are equal, but not c', function() {
-        expect(triangleIdentifier(1,1,2)).to.equal('Isosceles');
+        expect(triangleIdentifier(3,3,2)).to.equal('Isosceles');
     });
 
     it('is an isosceles triangle for a user input where side lengths a and c are equal, but not b', function() {
-        expect(triangleIdentifier(1,2,1)).to.equal('Isosceles');
+        expect(triangleIdentifier(3,2,3)).to.equal('Isosceles');
+    });
+
+    it('is an isosceles triangle for a user input where side lengths b and c are equal, but a', function() {
+        expect(triangleIdentifier(2,3,3)).to.equal('Isosceles');
     });
 });
