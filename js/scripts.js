6bf4da75b79184a8470f27d48085f99090947acd
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-10 10:00:42 -0700

    Fix spacing in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
 var triangleIdentifier = function(a, b, c) {
     if((a >= (b +c)) || (b >= (a + c)) || (c >= (a + b))) {
-        return "Not a triangle";
+      return "Not a triangle";
     } else {
-        if( a===b && a === c && b===c) {
-            return "Equilateral";
+      if( a===b && a === c && b===c) {
+          return "Equilateral";
         } else if ((a===b && a!==c && b!==c) || (a===c && a!==b && c!==b) || (b===c && a!==b && a!==c)) {
-            return "Isosceles";
+          return "Isosceles";
         } else if (a!==b && a!==c && b!==c) {
-            return "Scalene";
+          return "Scalene";
         }
     }
-
-
 }
