1cfb7bfccbba1d2275bfba0f916bb03899cf13ef
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-10 12:15:22 -0700

    Add triangle facts section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var triangleIdentifier = function(a, b, c) {
-    if((a >= (b +c)) || (b >= (a + c)) || (c >= (a + b))) {
-      return "unfortunately, dear user, not a triangle";
-    } else {
+  if((a >= (b +c)) || (b >= (a + c)) || (c >= (a + b))) {
+    return "unfortunately, dear user, not a triangle";
+  } else {
       if( a===b && a === c && b===c) {
-          return "equilateral triangle";
+        return "equilateral triangle";
       } else if ((a===b) || (a===c) || (b===c)) {
           return "isosceles triangle";
         } else {
@@ -14,17 +14,17 @@ var triangleIdentifier = function(a, b, c) {
 
 $(document).ready(function() {
   $('form#triangle').submit(function(event) {
-      debugger;
-     var a = parseInt($('input#side_a').val());
-     var b = parseInt($('input#side_b').val());
-     var c = parseInt($('input#side_c').val());
+    var a = parseInt($('input#side_a').val());
+    var b = parseInt($('input#side_b').val());
+    var c = parseInt($('input#side_c').val());
 
-     var result = triangleIdentifier(a, b, c);
+    var result = triangleIdentifier(a, b, c);
 
-     $('.triangle_result').empty();
-     $('.triangle_result').text(result);
+    $('.triangle_result').empty();
+    $('.triangle_result').text(result);
 
-     $('#result').show();
-     event.preventDefault();
+    $('#facts').hide();
+    $('#result').show();
+    event.preventDefault();
   });
 });
