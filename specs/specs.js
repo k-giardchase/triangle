9c94d88c6fa6b19025ed72cf96b7625a122f8e81
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-10 09:20:15 -0700

    Add second test for isosceles triangle

--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,10 @@
 describe('triangleIdentifier', function() {
+
     it('is an equilaterial triangle for a user input where all side lengths are equal', function() {
         expect(triangleIdentifier(1,1,1)).to.equal('Equilateral');
+    });
+
+    it('is an isosceles triangle for a user input where two side lengths are equal', function() {
+        expect(triangleIdentifier(1,2,1)).to.equal('Isosceles');
     });
 });
